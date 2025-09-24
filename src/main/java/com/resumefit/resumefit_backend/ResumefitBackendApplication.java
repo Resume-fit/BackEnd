6f96eefa3d6b8c9fcf1abd752ad108e574d66877
Go Yeongmin <ko07073@gmail.com>
2025-09-24 18:04:50 +0900

    first commit

---
+++ src/main/java/com/resumefit/resumefit_backend/ResumefitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.resumefit.resumefit_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumefitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumefitBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/resumefit/resumefit_backend/ResumefitBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resumefit.resumefit_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumefitBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
